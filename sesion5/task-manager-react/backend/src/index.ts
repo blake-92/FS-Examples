b933359c4f4740887b8a36b9116064bcc484a45f
Blake <62025564+blake-92@users.noreply.github.com>
2026-06-29 21:24:31 -0400

    feat(sesion5): servidor Express y ruta GET /tasks

--- sesion5/task-manager-react/backend/src/index.ts
+++ sesion5/task-manager-react/backend/src/index.ts
@@ -1,1 +1,28 @@
-console.log("Backend is running...");
+import express, { type Request, type Response } from "express";
+
+const app = express();
+const PORT = 3000;
+
+type Task = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+const tasks: Task[] = [
+  { id: 1, text: "Estudiar Node.js", completed: false },
+  { id: 2, text: "Crear servidor Express", completed: true },
+  { id: 3, text: "Probar rutas del backend", completed: false },
+];
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Backend is working!");
+});
+
+app.get("/tasks", (req: Request, res: Response) => {
+  res.json(tasks);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
